be50c6b1c2b5c2436d01a47645a807b57f4a6222
Coders Manic <manicstreetcoders@gmail.com>
2012-09-28 19:24:15 +0900

    a

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,4 +7,6 @@ class Question < ActiveRecord::Base
   attr_accessor :question_message
   attr_accessor :question_selection
   attr_accessor :question_answer
+
+  default_scope: :order => 'questions.number ASC'
 end
