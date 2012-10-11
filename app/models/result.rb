d9a044cc2a22eb3f979ee5f9d08702a7c73ff2dd
Coders Manic <manicstreetcoders@gmail.com>
2012-10-11 18:10:12 +0900

    a

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,10 @@
 class Result < ActiveRecord::Base
   attr_accessible :user_id_str, :exam_id, :question_id, :user_selection, :answer, :used_time
+
+  validates :user_id_str, :presence => true
+  validates :exam_id, :presence => true
+  validates :question_id, :presence => true
+  validates :answer, :presence => true
+  validates :used_time, :presence => true
+
 end
